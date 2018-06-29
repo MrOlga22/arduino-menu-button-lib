9c27e64de682dd5c11bd3be4d024903aaa704f31
MrOlga22 <36713373+MrOlga22@users.noreply.github.com>
2018-06-29 12:02:42 +0200

    Add files via upload
    beautified code

--- Menu.cpp
+++ Menu.cpp
@@ -5,15 +5,16 @@
 #include "Arduino.h"
 #include "Menu.h"
 
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
 //constructor
 Menu::Menu() {
 	//initialize instance of Adafruit_TFTLCS class called tft
-  	Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 	tft.reset();
 	uint16_t identifier = tft.readID(); //read lcd chip
 	tft.begin(identifier);
 	tft.setRotation(rotation);
-}	
+}
 
 Menu::setTextColor(char* color) {
 	_textColor = bitcolor(color);
@@ -43,14 +44,14 @@ int Menu::getBorder() { return _border; }
 
 //convert char to 16bit color code
 uint16_t Menu::bitcolor(char* color) {
-	uint16_t BLACK	= 0x0000;
-	uint16_t BLUE	= 0x001F;
-	uint16_t RED	= 0xF800;
-	uint16_t GREEN	= 0x07E0;
-	uint16_t CYAN	= 0x07FF;
-	uint16_t MAGENTA= 0xF81F;
-	uint16_t YELLOW	= 0xFFE0;
-	uint16_t WHITE	= 0xFFFF;
+	uint16_t BLACK	 = 0x0000;
+	uint16_t BLUE	   = 0x001F;
+	uint16_t RED	   = 0xF800;
+	uint16_t GREEN	 = 0x07E0;
+	uint16_t CYAN	   = 0x07FF;
+	uint16_t MAGENTA = 0xF81F;
+	uint16_t YELLOW	 = 0xFFE0;
+	uint16_t WHITE	 = 0xFFFF;
 	if (color == "BLACK") {
 		return BLACK;
 	} else if (color == "BLUE") {
@@ -72,7 +73,6 @@ uint16_t Menu::bitcolor(char* color) {
 
 //generate one button with given text, selected (1/0) and saved colors
 Menu::oneButton(char* b1, int sel) {
-  	Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 	uint16_t bC;
 	uint16_t tC;
 	if (sel == 0) {
@@ -91,14 +91,13 @@ Menu::oneButton(char* b1, int sel) {
 }
 //generate two buttons with given text, selected (2/1/0) and saved colors
 Menu::twoButtons(char* b1, char*b2, int sel) {
-  	Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 	tft.fillRect(0, 0, _width, _height, _backgroundColor);
 	tft.fillRect(_border, _border, _buttonwidth, _buttonheight, _buttonColor);
 	tft.setCursor(_border + textdistotop, _border + textdistotop);
 	tft.setTextColor(_textColor);
 	tft.setTextSize(textSize);
 	tft.println(b1);
-	tft.fillRect(_border, _xB2, _buttonwidth, _buttonheight, _buttonColor);	
+	tft.fillRect(_border, _xB2, _buttonwidth, _buttonheight, _buttonColor);
 	tft.setCursor(_border + textdistotop, _xB2 + textdistotop);
 	tft.setTextColor(_textColor);
 	tft.setTextSize(textSize);
@@ -114,19 +113,18 @@ Menu::twoButtons(char* b1, char*b2, int sel) {
 		tft.setCursor(_border + textdistotop, _xB2 + textdistotop);
 		tft.setTextColor(_buttonColor);
 		tft.setTextSize(textSize);
-		tft.println(b2);		
+		tft.println(b2);
 	}
 }
 //generate three buttons with given text, selected (3/2/1/0) and saved colors
 Menu::threeButtons(char* b1, char*b2, char* b3, int sel) {
-  	Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 	tft.fillRect(0, 0, _width, _height, _backgroundColor);
 	tft.fillRect(_border, _border, _buttonwidth, _buttonheight, _buttonColor);
 	tft.setCursor(_border + textdistotop, _border + textdistotop);
 	tft.setTextColor(_textColor);
 	tft.setTextSize(textSize);
 	tft.println(b1);
-	tft.fillRect(_border, _xB2, _buttonwidth, _buttonheight, _buttonColor);	
+	tft.fillRect(_border, _xB2, _buttonwidth, _buttonheight, _buttonColor);
 	tft.setCursor(_border + textdistotop, _xB2 + textdistotop);
 	tft.setTextColor(_textColor);
 	tft.setTextSize(textSize);
@@ -147,26 +145,24 @@ Menu::threeButtons(char* b1, char*b2, char* b3, int sel) {
 		tft.setCursor(_border + textdistotop, _xB2 + textdistotop);
 		tft.setTextColor(_buttonColor);
 		tft.setTextSize(textSize);
-		tft.println(b2);		
+		tft.println(b2);
 	} else if (sel == 3) {
-		tft.fillRect(_border, _xB3,_buttonwidth, _buttonheight, _textColor);	
+		tft.fillRect(_border, _xB3,_buttonwidth, _buttonheight, _textColor);
 		tft.setCursor(_border + textdistotop, _xB3 + textdistotop);
 		tft.setTextColor(_buttonColor);
 		tft.setTextSize(textSize);
 		tft.println(b3);
 	}
-	
 }
 //generate four buttons with given text, selected (4/3/2/1/0) and saved colors
 Menu::fourButtons(char* b1, char*b2, char* b3, char* b4, int sel) {
-  	Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 	tft.fillRect(0, 0, _width, _height, _backgroundColor);
 	tft.fillRect(_border, _border, _buttonwidth, _buttonheight, _buttonColor);
 	tft.setCursor(_border + textdistotop, _border + textdistotop);
 	tft.setTextColor(_textColor);
 	tft.setTextSize(textSize);
 	tft.println(b1);
-	tft.fillRect(_border, _xB2, _buttonwidth, _buttonheight, _buttonColor);	
+	tft.fillRect(_border, _xB2, _buttonwidth, _buttonheight, _buttonColor);
 	tft.setCursor(_border + textdistotop, _xB2 + textdistotop);
 	tft.setTextColor(_textColor);
 	tft.setTextSize(textSize);
@@ -176,7 +172,7 @@ Menu::fourButtons(char* b1, char*b2, char* b3, char* b4, int sel) {
 	tft.setTextColor(_textColor);
 	tft.setTextSize(textSize);
 	tft.println(b3);
-	tft.fillRect(_border, _xB4, _buttonwidth, _buttonheight, _buttonColor);	
+	tft.fillRect(_border, _xB4, _buttonwidth, _buttonheight, _buttonColor);
 	tft.setCursor(_border + textdistotop, _xB4 + textdistotop);
 	tft.setTextColor(_textColor);
 	tft.setTextSize(textSize);
@@ -192,9 +188,9 @@ Menu::fourButtons(char* b1, char*b2, char* b3, char* b4, int sel) {
 		tft.setCursor(_border + textdistotop, _xB2 + textdistotop);
 		tft.setTextColor(_buttonColor);
 		tft.setTextSize(textSize);
-		tft.println(b2);		
+		tft.println(b2);
 	} else if (sel == 3) {
-		tft.fillRect(_border, _xB3,_buttonwidth, _buttonheight, _textColor);	
+		tft.fillRect(_border, _xB3,_buttonwidth, _buttonheight, _textColor);
 		tft.setCursor(_border + textdistotop, _xB3 + textdistotop);
 		tft.setTextColor(_buttonColor);
 		tft.setTextSize(textSize);
@@ -204,7 +200,6 @@ Menu::fourButtons(char* b1, char*b2, char* b3, char* b4, int sel) {
 		tft.setCursor(_border + textdistotop, _xB4 + textdistotop);
 		tft.setTextColor(_buttonColor);
 		tft.setTextSize(textSize);
-		tft.println(b4);		
+		tft.println(b4);
 	}
 }
-	
--- Menu.h
+++ Menu.h
@@ -27,37 +27,36 @@
 
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
 
-
 class Menu {
   private:
-	int _width = displaywidth;
-	int _height = displayheight;
-	int _buttonheight = _height / 4 - 10; 
-	int _buttonwidth =  _width - 16;
-	int _border = 8;
-	int _textColor;
-	int _buttonColor;
-	int _backgroundColor;
-	int _xB2 = 2 * _border + _buttonheight;
-	int _xB3 = 3 * _border + 2 * _buttonheight;
-	int _xB4 = 4 * _border + 3 * _buttonheight;
-	static uint16_t bitcolor(char*);
+	  int _width = displaywidth;
+	  int _height = displayheight;
+	  int _buttonheight = _height / 4 - 10;
+	  int _buttonwidth =  _width - 16;
+	  int _border = 8;
+	  int _textColor;
+	  int _buttonColor;
+	  int _backgroundColor;
+	  int _xB2 = 2 * _border + _buttonheight;
+	  int _xB3 = 3 * _border + 2 * _buttonheight;
+	  int _xB4 = 4 * _border + 3 * _buttonheight;
+	  static uint16_t bitcolor(char*);
   public:
-	Menu();
-	setTextColor(char* color);
-	setButtonColor(char* color);
-	setBackgroundColor(char* color);
-	setColors(char* text, char* button, char* bg);
-	uint16_t getTextColor();
-	uint16_t getButtonColor();
-	uint16_t getBackgroundColor();
-	int getButtonHeight();
-	int getButtonWidth();
-	int getBorder();
-	oneButton(char* b1, int sel);
-	twoButtons(char* b1, char*b2, int sel);
-	threeButtons(char* b1, char*b2, char* b3, int sel);
-	fourButtons(char* b1, char*b2, char* b3, char* b4, int sel);
+	  Menu();
+	  setTextColor(char* color);
+	  setButtonColor(char* color);
+	  setBackgroundColor(char* color);
+	  setColors(char* text, char* button, char* bg);
+	  uint16_t getTextColor();
+	  uint16_t getButtonColor();
+	  uint16_t getBackgroundColor();
+	  int getButtonHeight();
+	  int getButtonWidth();
+	  int getBorder();
+	  oneButton(char* b1, int sel);
+	  twoButtons(char* b1, char*b2, int sel);
+	  threeButtons(char* b1, char*b2, char* b3, int sel);
+	  fourButtons(char* b1, char*b2, char* b3, char* b4, int sel);
 };
 
 #endif
